9493988a53a846df550b96d444369cd7285a45a7
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-29 15:32:08 -0400

    pass tests

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const calculate = (arr) => {
-	console.log(arr)
+  const filteredArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === null) {
+      arr[i] = 0
+      filteredArr.push(arr[i])
+    } else if (arr[i] !== '' && arr[i] !== undefined) {
+      filteredArr.push(arr[i])
+    }
+  }
+
+  if (filteredArr.length < 3) { return NaN }
+
+  const n1 = Number(filteredArr[0])
+  const n2 = Number(filteredArr[2])
+
+  switch (filteredArr[1]) {
+    case '+':
+      return n1 + n2
+    case '-':
+      return n1 - n2
+    case '*':
+      return n1 * n2
+    case '/':
+      return n1 / n2
+    default:
+      return NaN
+  }
 }
 
 module.exports = calculate
